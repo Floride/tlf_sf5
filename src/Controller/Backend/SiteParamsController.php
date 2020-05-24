7ad372fc5a6379be004eb8abcbe2630d38c28132
Sylvain FLORIDE <floride_sylvain@hotmail.com>
2020-05-24 21:24:20 +0200

    developpement #20200524-2
    Modifiaction CRUD Site_Params
    Ajout lien Nouveau Joueur

--- src/Controller/Backend/SiteParamsController.php
+++ src/Controller/Backend/SiteParamsController.php
@@ -4,12 +4,14 @@ namespace App\Controller\Backend;
 
 use DateTime;
 use App\Entity\SiteParams;
+use App\Form\SiteParamType;
 use App\Repository\SiteParamsRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Class AdminController
@@ -49,15 +51,34 @@ class SiteParamsController extends AbstractController
     }
 
     /**
+     * Editer un paramètre du site
+     *
+     * @param Request $request
+     * @param SiteParams $siteParams
+     *
+     * @return Response
      * @Route("/site/params/{id}/edit", name="site_params_edit", requirements={"id"="\d+"},  methods={"GET", "POST"})
      */
-    public function edit(): Response
+    public function edit(Request $request, SiteParams $siteParams): Response
     {
-        $params = $this->paramsRepository->findAll();
+        $param = $this->paramsRepository->findOneBy(['id' => $siteParams->getId()]);
         
-        return $this->render('admin/site_params/index.html.twig', [
+        $form = $this->createForm(SiteParamType::class, $param);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush(); // Mise à jour du joueur en BDD
+            $this->addFlash(
+                'success',
+                'Le paramètre a bien été modifié.'
+            );
+            return $this->redirect($this->generateUrl('site_params_list')); // redirection vers la liste des Paramètres de site
+        }
+
+        return $this->render('admin/site_params/edit.html.twig', [
             'controller_name' => 'SiteParamsController',
-            'params' => $params
+            'form' => $form->createView(),
+            'param' => $param,
         ]);
     }
 
@@ -71,6 +92,40 @@ class SiteParamsController extends AbstractController
         return $this->render('admin/site_params/index.html.twig', [
             'controller_name' => 'SiteParamsController',
             'params' => $params
+        ]);
+    }
+    
+
+    /**
+     * Créer un paramètre du site
+     *
+     * @param Request $request
+     * @param SiteParams $siteParams
+     *
+     * @return Response
+     * @Route("/site/params/new", name="site_params_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $param = new SiteParams();
+        
+        $form = $this->createForm(SiteParamType::class, $param);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($param);  // On persiste l'objet $param
+            $this->manager->flush(); // Mise à jour du joueur en BDD
+            $this->addFlash(
+                'success',
+                'Le paramètre a bien été créé.'
+            );
+            return $this->redirect($this->generateUrl('site_params_list')); // redirection vers la liste des Paramètres de site
+        }
+
+        return $this->render('admin/site_params/new.html.twig', [
+            'controller_name' => 'SiteParamsController',
+            'form' => $form->createView(),
+            'param' => $param,
         ]);
     }
 }
